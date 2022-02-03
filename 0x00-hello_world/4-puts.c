f93e7a115b4b2cc957fa879c1fc861e18b20c42e
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-03 08:49:25 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Ejercicio 4
+ * main - prints the phrase
  *
- * Return: Always 0 (Success)
+ * Return: returns no error
  */
 int main(void)
 {
-
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
