99b83f3979ac128e3f1dfd0da2da16736ea08fc2
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-04 10:12:55 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+int b;
+
+for (a = 0 ; a <= 9; a++)
+{
+for (b = 0 ; b <= 9 ; b++)
+{
+if ((a != b) && (a < b))
+{
+putchar(a % 10 + '0');
+putchar(b % 10 + '0');
+if (a != 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
