ec5976f78bbf8d75fc7be8b71f1befa91860edfe
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-04 06:27:44 -0800

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+*main - prints out a value depending on the number
+*
+*Return: no error
+*/
+int main(void)
+{
+srand(time(NULL));
+int r = rand();
+int r2 = rand() % 10;
+
+if (r2 >= 5)
+{
+r = (r * -1);
+}
+
+if (r > 0)
+{
+printf("%d is positive\n", r);
+}
+else if (r == 0)
+{
+printf("%d is zero\n", r);
+}
+else
+{
+printf("%d is negative\n", r);
+}
+return (0);
+}
