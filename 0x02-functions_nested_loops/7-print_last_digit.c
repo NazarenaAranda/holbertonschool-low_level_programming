5a7d9308b57a1ea8ab6e2d5503c8c148e0fe5ba3
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-07 10:53:12 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_last_digit - ad
+* @c: num
+* Return: last digit
+*/
+int print_last_digit(int c)
+{
+int x;
+if (c == -2147483648)
+c = 88;
+if (c < 0)
+{
+c = -c;
+}
+x = c % 10;
+_putchar(x % 10 + '0');
+return (x);
+}
