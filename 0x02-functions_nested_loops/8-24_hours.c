f2dfdf25bb6e285fbc9def1522d95f27880140ab
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-07 17:42:54 -0800

    8-24 solution

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int jack_bauer(void)
+{	
+    int i;
+    int j;
+    int k;
+    int l;
+
+    for(i='0';i<='2';i++){
+    for(j='0';j<='9';j++){
+	    if(i=='2' && j=='4'){
+                        return(0);
+                }
+    	for(k='0';k<='5';k++){
+    	for(l='0';l<='9';l++){
+		_putchar(i);
+		_putchar(j);
+		_putchar(':');
+		_putchar(k);
+		_putchar(l);
+		_putchar('\n');
+    }
+    }
+    }
+    }
+    return (0);
+}
