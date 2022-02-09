c4f7eec8785fd7e972e38d0298a8f328aaf4ff22
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-09 13:39:57 -0800

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- 	*print_numbers - Prints out a string to 10.
+	*print_numbers - Prints out a string to 10.
 	*Return: 0 if success.
 	*/
 void print_numbers(void)
 {
 	int n;
+
 	for (n = '0' ; n <= '9' ; n++)
 	{
 	_putchar(n);
