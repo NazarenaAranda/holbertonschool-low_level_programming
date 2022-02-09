372dc6c814232cb082582cdc59c7329bcfce34f5
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-09 05:47:04 -0800

    two intent

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+}
