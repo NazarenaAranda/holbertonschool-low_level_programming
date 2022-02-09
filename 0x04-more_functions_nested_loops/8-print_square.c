13538c6b264b831231648c876ae15a7b80180c87
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-09 14:31:14 -0800

    9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+	*print_square - Prints out a string to 10.
+	*@size: First variable
+	*/
+void print_square(int size)
+{
+	int l;
+	int a;
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for (l = 1; l <= size; l++)
+	{
+	for (a = 1; a <= size; a++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+}
