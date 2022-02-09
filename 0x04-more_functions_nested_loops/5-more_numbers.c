2c4cb13025c0bda3d0e6c7e5322f0968bf4e7ed2
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-09 14:02:20 -0800

    8

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+	*more_numbers - Prints out a string to 10.
+	*Return: 0 if success.
+	*/
+void more_numbers(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+
+	_putchar(y / 10 + '0');
+	_putchar(y % 10 + '0');
+
+	}
+	_putchar('\n');
+	}
+}
