11c2227c6458b2115c36a86881d31933641ca268
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-11 14:42:31 -0800

    2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _puts - ad
+ * @str: ada
+ */
+void _puts(char *str)
+{
+int i, cont = 0;
+char l = '0';
+if (*str == '\0')
+{
+_putchar('\n');
+}
+else
+{
+while (l != '\0')
+{
+cont++;
+l = *(str + cont);
+}
+for (i = 0; i < cont; i++)
+{
+l = *(str + i);
+_putchar(l);
+}
+_putchar('\n');
+}
+}
