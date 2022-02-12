688c7dec10c13ce9c43561e2f312ce8a8e2b446f
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-11 16:14:50 -0800

    exercises

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * puts_half - ad
+ * @str: string
+ */
+void puts_half(char str)
+{
+int largo = _strlen(str);
+int i = 0;
+if (largo % 2 == 0)
+{
+for (i = largo / 2; i < largo; i++)
+{
+_putchar(*(str + i));
+}
+}
+else
+{
+i = (largo - 1) / 2;
+for (i = i + 1; i < largo; i++)
+{
+_putchar(*(str + i));
+}
+}
+_putchar('\n');
+}
+/**
+ * _strlen - ada
+ * @s: das
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int cont = 0;
+char l = '0';
+if (*s == '\0')
+return (0);
+while (l != '\0')
+{
+cont++;
+l = *(s + cont)
+}
+return (cont);
+}
