f9b32a17a3760c09ba1a3d8bb678c23b89f7e852
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-16 11:44:54 -0800

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _strncat- Esta funcion es parecida a _strcat
+* @dest:puntero 2
+* @src: puntero 1
+* @n:bytes
+* Return: puntero a dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i = 0;
+	int c = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[c] != '\0' && c < n)
+	{
+		dest[i + c] = src[c];
+			c++;
+	}
+
+	dest[i + c] = '\0';
+
+	return (dest);
+}
