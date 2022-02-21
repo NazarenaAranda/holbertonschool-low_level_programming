a92a38f24f1a27e88d71ef80aa4bd14edb272b76
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-02-21 09:13:34 -0800

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,20 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 unsigned int i = 0;
 
-while (i < n && (dest[i]) != '\0')
-
+for (i = 0; i < n; i++)
 {
-	if (*src != '\0')
-		*(dest + i) = *src++;
-
-	else
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
-
+	dest[i] = src[i];
 }
-
 return (dest);
-
 }
+
