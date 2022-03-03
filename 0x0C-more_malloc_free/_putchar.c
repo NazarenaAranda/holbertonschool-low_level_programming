73042deb9fe27652c56576838063261e4af0f663
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-03-03 10:22:07 -0800

    0

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+#incluir <unistd.h>
+
+/**
+ * _putchar - escribe el carácter c en stdout
+ * @c: El carácter a imprimir
+ *
+ * Retorno: Sobre el éxito 1.
+ * En caso de error, se devuelve -1 y errno se establece correctamente.
+ */
+int _putchar(char c)
