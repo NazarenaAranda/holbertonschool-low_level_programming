5e724ea1c6ce7e13fb3d04fd624227b78a57652b
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-03-08 13:54:01 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef TYPE_H
+#define TYPE_H
+/**
+* struct dog - fvsd
+* @name: sdjdf
+* @age: sadj
+* @owner: kdjwvb
+*/
+
+struct dog
+{
+	char * name; 
+	float age;
+	char * owner;
+};
+#endif
