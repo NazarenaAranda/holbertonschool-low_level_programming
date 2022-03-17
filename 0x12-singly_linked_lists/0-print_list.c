9f5df96d293a33da88b68226b9a7237e420e63b8
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-03-17 13:32:58 -0700

    prints all the elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+* print_list - Print all elements of a list_a
+* @h: Head
+* Return: node
+*/
+size_t print_list(const list_t *h)
+{
+
+	size_t  a = 0;
+	const list_t *n;
+
+	n = h;
+
+	while (n != NULL)
+	{
+		if (n->str)
+			printf("[%d] %s\n", n->len, n->str);
+		else
+			printf("[0] (nil)\n");
+		n = n->next;
+		a++;
+	}
+	return (a);
+}
