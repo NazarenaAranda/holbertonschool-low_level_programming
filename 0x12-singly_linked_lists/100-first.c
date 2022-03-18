384c219690b620af78db6398f76c6958d88c7fbe
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-03-17 19:25:13 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-void __attribute__((constructor)) hare(void);
+
+void __attribute__((constructor)) lie(void);
 /**
-* lieb - Prints a string
+* lie - Prints a string
+*
 */
-void lieb(void)
+void lie(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");
