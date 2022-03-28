37f964ed08523d1aee2344e8e44e6d5590e5c09f
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-03-28 15:27:17 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	guardar = malloc(contador * sizeof(char));
 
-	abrir = open(filename, O_CREAT | O_WRONLY | O_RDONLY, 600);
+	abrir = open(filename, O_WRONLY | O_RDONLY, 600);
 	leer = read(abrir, guardar, contador);
 	escribir = write(STDOUT_FILENO, guardar, leer);
 
