92dea4738670393064e1c856fe53132bcf0f0be5
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-04-15 14:53:42 -0700

    convertir a binario

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - convertit numero binarioo a entero unsigned
+ * @b: puntero al primer caracter de string
+ * Return: 0 o numero convertido
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int convertido;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+	{
+	while (b[i] >= 'a' && b[i] <= 'z')
+		return (0);
+
+	convertido <<= 1;
+	convertido = convertido + b[i] - '0';
+	i++;
+	}
+	return (convertido);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef BIT
+#define BIT
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
+
