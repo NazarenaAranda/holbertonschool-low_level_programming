b1285ef162b69291d8dc881d9b0f503992ec43ec
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-04-15 15:12:27 -0700

    por fin

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,25 +6,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int convertido = 0;
-	unsigned int aux = 1;
-	int i = 0, j = 0;
+
+	unsigned int convertid = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
 
 	while (b[i])
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
-	}
+	while (b[i] >= 'a' && b[i] <= 'z')
+		return (0);
 
-	for (j = i - 1; j >= 0; j--)
-	{
-		if (b[j] == '1')
-			convertido += aux;
+	convertid <<= 1;
+	convertid = convertid + b[i] - '0';
+	i++;
 	}
-
-	return (convertido);
+	return (convertid);
 }
