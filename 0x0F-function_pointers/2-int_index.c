08e86d9004ebd21eea819c962e78bfd7f037e3a3
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-04-17 14:58:19 -0700

    1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - funcion que busca entero
+ * @array: puntero al inicio de la array
+ * @size: tamaño
+ * @cmp: comparar
+ * Return: si hay un error -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a = 0;
+
+	if (array == NULL || cmp == NULL || size < 0 )
+		return (-1);
+
+	while (a < size)
+	{
+		a++;
+		if (cmp(array[a]) != 0)
+			return (a);
+	}
+	return (-1);
+}
