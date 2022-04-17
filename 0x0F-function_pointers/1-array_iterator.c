eac0767a8f30fdf2d215f2701ce812489e0d0b03
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-04-17 14:42:06 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - funcion para ejecutar
+* otra funcion
+ * @array: puntero al inicio de la array
+ * @size: tamaño de la array
+ * @action: accion para ejecutar contenido
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a = 0;
+
+	if (array == NULL)
+		return;
+
+	if (action == NULL)
+		return;
+
+	while (a < size)
+	{
+		 action(array[a]);
+		 a++;
+	}
+}
