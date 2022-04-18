3674e3480fa0ab15f01aedf70bf67d503ea463ed
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-04-18 21:49:32 +0000

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,5 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* _calloc - a memory allocator
-* 
+ * _calloc - a memory allocator
+ * @nmemb: elementos
+ * @size: variable bytes
+ * Return: ret
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int a;
+	char *str;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	str = malloc(nmemb * size);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (a = 0; a < nmemb * size; a++)
+		str[a] = 0;
+
+	return (str);
+}
