6cc1497220aea832c09eca43d34fb5669777ac18
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-06-07 06:58:58 -0700

    suma de elementos

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - retorna la suma de todos los datos
+ * @head: head
+ * Return: la suma de todos los datos (n) o si la lista está vacía, devuelve 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int count = 0;
+
+	while (head)
+	{
+		count += head->n;
+		head = head->next;
+	}
+	return (count);
+}
