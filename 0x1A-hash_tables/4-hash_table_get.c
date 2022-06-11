6ddda648b2aa0fa1270a4e7025d747417be7eed4
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-06-11 01:10:31 -0700

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - recuperar un valor de una clave
+ * @ht: hash table a consultar
+ * @key: key es la clave que se busca
+ * Return: el valor asociado al elemento, o NULL si no se ha podido encontrar la clave
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (!ht || !key)
+		return NULL;
+	
+	index = key_index((unsigned char *)key, ht->size);
+	
+	while (ht->array[index] != NULL)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+
+	return (0);
+}
